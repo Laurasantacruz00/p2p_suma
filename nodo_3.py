bd99667f2436f8821ab6fd6c472be25c4b5a15c7
Laura Santacruz <32122480+Laurasantacruz00@users.noreply.github.com>
2020-06-03 16:36:39 -0500

    Update nodo_3.py

--- nodo_3.py
+++ nodo_3.py
@@ -4,7 +4,7 @@ sys.path.insert(0, '..') # Import the files where the modules are located
 
 from main_nodo import nodo
 
-node_3 = nodo("158.69.63.154", 8003)
+node_3 = nodo("127.0.0.1", 8003)
 
 time.sleep(1)
 
@@ -12,8 +12,8 @@ node_3.start()
 
 time.sleep(1)
 
-node_3.connect_with_node('142.44.246.12', 8004)
-node_3.connect_with_node('142.44.246.92', 8001)
+node_3.connect_with_node('127.0.0.1', 8004)
+node_3.connect_with_node('127.0.0.1', 8001)
 
 time.sleep(1)
 
@@ -21,19 +21,20 @@ while True:
     try: 
         print("\n_____________BIENVENIDO___________\n"
         +"\n Ingrese el numero de la acción que desea realizar:"
-        +"\n1. Agregar numero \n2.Sumar numero \n3. Salir")
+        +"\n1. Agregar numero \n2. Sumar numero \n3. Salir")
         
         op = int(input(" \n : "))
 
         if op == 1:
             num = input("\nEscriba el numero que desea agregar  ")
-            archivo = open("numeros.txt","a")#Creando txt con la información del cliente
+            archivo = open("numeros_1.txt","a")#Creando txt con la información del cliente
             archivo.write("{},0".format(num))
             archivo.close() 
             print("Numero guardado")
-            node_3.send_to_nodes(num)
         elif op == 2:
-            numbers = []
+            node_3.send_to_nodes("true")
+            time.sleep(2)
+            ''' numbers = []
             with open("numeros.txt", "r") as file:
                 for line in file:
                     fields = line.split(",")
@@ -41,7 +42,7 @@ while True:
                 subnumbers = (int(field) for field in fields)
                 numbers.extend(subnumbers)
                 suma = sum(numbers)
-            print("\nLa suma es: "+str(suma))
+            print("\nLa suma es: "+str(suma)) '''
         elif op == 3:
             node_3.stop()
             print('end')
