a6f1dc474d81c24f072e26b9de5986b0e2988d07
Laura Santacruz <32122480+Laurasantacruz00@users.noreply.github.com>
2020-06-03 16:30:09 -0500

    Update nodo_1.py

--- nodo_1.py
+++ nodo_1.py
@@ -4,50 +4,48 @@ sys.path.insert(0, '..') #Importando archivos donde se encuentran los modulos
 
 from main_nodo import nodo
 
-node_1 = nodo("142.44.246.92", 8001)#Creación de nodo
+node_1 = nodo("127.0.0.1", 8001)
 
-time.sleep(1)#Retrazando ejecución
+time.sleep(1)
 
-node_1.start()#Iniciando hilo
+node_1.start()
 
-time.sleep(2)#Retrazando ejecución
+time.sleep(2)
 
-node_1.connect_with_node('142.44.246.23', 8002)#Conectando con nodo 2
+node_1.connect_with_node('127.0.0.1', 8002)
 
-time.sleep(1)#Retrazando ejecución
+time.sleep(1)
 
 while True:
     try: 
-        #Menu
         print("\n_____________BIENVENIDO___________\n"
         +"\n Ingrese el numero de la acción que desea realizar:"
         +"\n1. Agregar numero \n2. Sumar numero \n3. Salir")
         
-        op = int(input(" \n : "))#Opción elegida por el usuario 
+        op = int(input(" \n : "))
 
         if op == 1:
-            num = input("\nEscriba el numero que desea agregar  ")#Numero a agregar 
-            archivo = open("numeros.txt","a")#Guardando numero en un txt
+            num = input("\nEscriba el numero que desea agregar  ")
+            archivo = open("numeros_1.txt","a")#Creando txt con la información del cliente
             archivo.write("{},0".format(num))
             archivo.close() 
             print("Numero guardado")
-            node_1.send_to_nodes(num)#Enviando numero a los otros nodos 
         elif op == 2:
-            numbers = []
+            node_1.send_to_nodes("true")
+            time.sleep(2)
+            ''' numbers = []
             with open("numeros.txt", "r") as file:
                 for line in file:
-                    fields = line.split(",")#Recorriendo archivo txt
+                    fields = line.split(",")
                     #print(line.rstrip("n"))
-                subnumbers = (int(field) for field in fields)#Separando numeros
+                subnumbers = (int(field) for field in fields)
                 numbers.extend(subnumbers)
-                suma = sum(numbers)#Sumando numeros
-            print("\nLa suma es: "+str(suma))#Respuesta
+                suma = sum(numbers)
+            print("\nLa suma es: "+str(suma)) '''
         elif op == 3:
-            #Deteniendo nodo
             node_1.stop()
             print('end')
-            break#Rompiendo ciclo
+            break
     except ValueError:
-        #Control de errores
         print("Porfavor, ingresa solo numeros")
     
